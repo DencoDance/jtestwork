3b2c162579243d82a2361a9894a7dc27a1d33c2f
Yevhenii <mihailovskiyeugeny@gmail.com>
2019-02-11 14:30:46 +0200

    register form validation

--- frontendtest/src/components/RegisterForm.vue
+++ frontendtest/src/components/RegisterForm.vue
@@ -1,6 +1,13 @@
 <template>
-  <form class="loginForm">
+  <form class="loginForm" @submit="checkForm">
     <div class="form-group">
+      <p v-if="errors.length">
+        <b>Пожалуйста исправьте указанные ошибки:</b>
+        <ul>
+      <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
       <div class="row">
         <div class="col-sm-12 col-md-4 left">
           <label for="loginfirstname">
@@ -13,6 +20,8 @@
             id="loginfirstname"
             class="form-control"
             type="text"
+            v-model="firstName"
+            name="firstName"
           />
         </div>
       </div>
@@ -30,6 +39,27 @@
             id="loginlastname"
             class="form-control"
             type="text"
+            v-model="secondName"
+            name="secondName"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 left">
+          <label for="birthDate">
+            <span class>Date of Birth</span>
+          </label>
+        </div>
+        <div class="col-sm-12 col-md-8">
+          <input
+            id="birthDate"
+            class="form-control"
+            type="date"
+            v-model="age"
+            name="age"
+            min="0"
           />
         </div>
       </div>
@@ -47,6 +77,9 @@
             id="loginemail"
             class="form-control"
             type="email"
+            
+            v-model="email"
+            name="email"
           />
         </div>
       </div>
@@ -64,6 +97,8 @@
             id="loginphone"
             class="form-control"
             type="tel"
+            v-model="userNumber"
+            name="userNumber"
           />
         </div>
       </div>
@@ -81,6 +116,9 @@
             id="loginpass"
             class="form-control"
             type="password"
+            
+            v-model="userPassword"
+            name="userPassword"
           />
         </div>
       </div>
@@ -98,6 +136,8 @@
             id="loginpass"
             class="form-control"
             type="password"
+            v-model="userConfirmPassword"
+            name="userConfirmPassword"
           />
         </div>
       </div>
@@ -109,5 +149,51 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      errors: [],
+      firstName: null,
+      secondName: null,
+      email: null,
+      userNumber: null,
+      userPassword: null,
+      userConfirmPassword: null
+      
+    };
+  },
+  methods: {
+    checkForm: function(e) {
+      if (this.firstName && this.age && this.secondName && this.email && this.userNumber && this.userPassword && (this.userPassword == this.userConfirmPassword)) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.firstName) {
+        this.errors.push("Требуется указать имя.");
+      }
+      if (!this.age) {
+        this.errors.push("Требуется указать возраст.");
+      }
+      if(!this.secondName) {
+        this.errors.push ("Требуется указать фамилию.");
+      }
+      if(!this.email) {
+        this.errors.push ("Требуется указать почтовый адрес.");
+      }
+      if(!this.userNumber) {
+        this.errors.push ("Требуется указать номер телефона.");
+      }
+      if(!this.userPassword) {
+        this.errors.push ("Требуется указать пароль.");
+      }
+      if(this.userPassword !== this.userConfirmPassword) {
+        this.errors.push("Пароли не совпадают.");
+      }
+
+      e.preventDefault();
+    }
+  }
+};
 </script>
